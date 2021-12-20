18c4a4052aa2e04435469ab73b2ff3f63585ba6f
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2021-12-20 20:38:28 +0300

    feat: validatorPage complete

---
+++ src/lib/ValidatorPage/ValidatorItem/index.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+	export let first: boolean = false;
+	export let validatorName: string = 'Arcadia';
+	export let uptime = 1;
+	export let validatorCommission = 0.05;
+	export let votingPower = 1;
+	export let selfDelegation = 1;
+	export let delegationReturn = 0.405;
+</script>
+
+<div class="main" class:top-border={!first}>
+	<div class="header">
+		<h4>{validatorName}</h4>
+		<p class="delegate">Delegate</p>
+	</div>
+	<div class="details-holder">
+		<div class="detail left">
+			<p class="detail-title">Uptime</p>
+			<p class="detail-value">{uptime * 100} %</p>
+		</div>
+		<div class="detail mid">
+			<p class="detail-title">Validator Commission</p>
+			<p class="detail-value">{validatorCommission * 100} %</p>
+		</div>
+		<div class="detail right">
+			<p class="detail-title">Voting power</p>
+			<p class="detail-value">{votingPower * 100} %</p>
+		</div>
+		<div class="detail left">
+			<p class="detail-title">Self-delegation</p>
+			<p class="detail-value">{selfDelegation * 100} %</p>
+		</div>
+		<div class="detail mid">
+			<p class="detail-title">Delegation Return</p>
+			<p class="detail-value">{delegationReturn * 100} %</p>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	.main {
+		@apply w-full pt-7 pb-8 pl-5;
+	}
+
+	h4 {
+		@apply text-[18px] font-extrabold mr-5;
+	}
+
+	.header {
+		@apply flex mb-5;
+	}
+
+	.delegate {
+		@apply px-3 py-1 text-white bg-light-appPurple rounded-[46px] text-xs;
+	}
+
+	.details-holder {
+		@apply flex flex-wrap;
+	}
+
+	.detail {
+		@apply w-1/3 flex text-xs mb-1;
+	}
+
+	.detail-title {
+		@apply text-light-gray min-w-max;
+	}
+
+	.detail-value {
+		@apply w-full text-right max-w-full mr-8;
+	}
+
+	.top-border {
+		@apply border-t border-black border-opacity-10;
+	}
+</style>
---
+++ src/lib/ValidatorPage/index.svelte
@@ -0,0 +1,63 @@
+<script>
+	import ValidatorItem from './ValidatorItem/index.svelte';
+</script>
+
+<div class="main">
+	<h3>Validators</h3>
+	<div class="validator-holder">
+		<ValidatorItem first={true} />
+		<ValidatorItem />
+		<ValidatorItem />
+		<ValidatorItem />
+		<ValidatorItem />
+		<ValidatorItem />
+		<ValidatorItem />
+		<ValidatorItem />
+		<ValidatorItem />
+		<ValidatorItem />
+		<ValidatorItem />
+	</div>
+	<div class="button-holder">
+		<button>Show more</button>
+	</div>
+</div>
+
+<style lang="postcss">
+	/* width */
+	::-webkit-scrollbar {
+		@apply w-[7px];
+	}
+
+	/* Track */
+	::-webkit-scrollbar-track {
+		@apply rounded-[39px];
+	}
+
+	/* Handle */
+	::-webkit-scrollbar-thumb {
+		@apply bg-light-scrollBar rounded-[39px];
+	}
+
+	.main {
+		@apply h-screen flex flex-col;
+	}
+
+	h3 {
+		@apply font-bold text-2xl;
+	}
+
+	.validator-holder {
+		@apply w-[650px] overflow-y-auto h-[85%] pr-6;
+	}
+
+	.button-holder {
+		@apply w-[650px] flex items-center justify-center;
+	}
+
+	button {
+		@apply border-2 border-light-lineColor rounded-[90px];
+		@apply text-sm font-bold;
+		@apply my-6 py-2 px-4;
+		@apply hover:bg-light-appPurple hover:text-white hover:border-light-appPurple transition duration-500;
+	}
+</style>
