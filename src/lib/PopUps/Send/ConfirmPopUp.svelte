fff4cd1ad17fc1a4a5e6bd159b7d719ab4f3e063
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2021-12-23 01:47:52 +0300

    add: Send Popups

---
+++ src/lib/PopUps/Send/ConfirmPopUp.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Popup from '$lib/Common/Popup.svelte';
+
+	export let amount: number = 50;
+	export let address: string = '4e32r8932hdi32hurh8hri3hior2io3r32rj3iofj';
+</script>
+
+<Popup title="Send CSPR" on:confirm on:cancel>
+	<p>
+		You are about to send <span class="highlights">{amount} CSPR</span>
+		to
+		<span class="highlights">{address}</span>
+		on the CSPR network.
+	</p>
+</Popup>
+
+<style lang="postcss">
+	p {
+		@apply text-sm font-semibold;
+	}
+
+	.highlights {
+		@apply text-light-appPurple break-words;
+	}
+</style>
