f4be8bf108714b57a32f676325a83b915ce1363c
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2021-12-23 01:59:17 +0300

    add: dark Overlay

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
 	import '$styles/tailwind.css';
+	import { fade } from 'svelte/transition';
+	import { darkOverlay } from '$stores/popupStore';
 </script>
 
-<slot />
+<main>
+	{#if $darkOverlay}
+		<div transition:fade class="darker" />
+	{/if}
+	<slot />
+</main>
+
+<style lang="postcss">
+	.darker {
+		@apply fixed top-0 bottom-0 left-0 right-0 z-30 bg-[#00000077];
+	}
+</style>
