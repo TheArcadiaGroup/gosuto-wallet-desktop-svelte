2d7a3cbe15c767e58a3af0cd92b31b3ae08938a4
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2021-12-23 03:12:30 +0300

    claim popups

---
+++ src/lib/PopUps/Claim/SuccessPopup.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Popup from '$lib/Common/Popup.svelte';
+
+	export let amount: number = 15;
+</script>
+
+<Popup title="Success!" on:confirm on:cancel>
+	<p>
+		<span class="highlights">{amount} CSPR</span>
+		Claimed
+	</p>
+</Popup>
+
+<style lang="postcss">
+	p {
+		@apply font-semibold;
+	}
+
+	.highlights {
+		@apply text-light-appPurple break-words;
+	}
+</style>
