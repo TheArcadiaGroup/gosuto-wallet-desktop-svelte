8071399fe4b7ab14d6616d74fbdfdbc1b596fd7d
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2021-12-23 03:13:57 +0300

    add: utils for date conversion and calculations

---
+++ src/utils/index.ts
@@ -0,0 +1,70 @@
+const monthNames = [
+	'Jan',
+	'Feb',
+	'Mar',
+	'Apr',
+	'May',
+	'Jun',
+	'Jul',
+	'Aug',
+	'Sep',
+	'Oct',
+	'Nov',
+	'Dec',
+];
+
+export const convertDate = (date: Date): string => {
+	return `${date.getDate()} ${monthNames[date.getMonth()]} ${date.getFullYear()}`;
+};
+
+export const dateDifference = (larger: Date, smaller: Date): string => {
+	// Discard the time and time-zone information.
+	const utc1 = Date.UTC(
+		smaller.getFullYear(),
+		smaller.getMonth(),
+		smaller.getDate(),
+		smaller.getHours(),
+		smaller.getMinutes(),
+		smaller.getSeconds(),
+	);
+	const utc2 = Date.UTC(
+		larger.getFullYear(),
+		larger.getMonth(),
+		larger.getDate(),
+		larger.getHours(),
+		larger.getMinutes(),
+		larger.getSeconds(),
+	);
+
+	return convertMiliseconds(Math.floor(utc2 - utc1), 'default');
+};
+
+function convertMiliseconds(miliseconds: number, format: string): string {
+	const total_seconds = Math.floor(miliseconds / 1000);
+	const total_minutes = Math.floor(total_seconds / 60);
+	const total_hours = Math.floor(total_minutes / 60);
+	const days = Math.floor(total_hours / 24);
+
+	const seconds = total_seconds % 60;
+	const minutes = total_minutes % 60;
+	const hours = total_hours % 24;
+
+	const pad = function (n: Number) {
+		return n < 10 ? '0' + n : n;
+	};
+
+	switch (format) {
+		case 's':
+			return `${total_seconds}`;
+		case 'm':
+			return `${total_minutes}`;
+		case 'h':
+			return `${total_hours}`;
+		case 'd':
+			return `${days}`;
+		case 'default':
+			return `${days}:${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+		default:
+			return `${days}:${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+	}
+}
