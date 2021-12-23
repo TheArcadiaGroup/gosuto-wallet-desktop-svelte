814b9edb1fa6c859d6746c96577a374d7aa61d67
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2021-12-23 03:13:26 +0300

    stake popups

---
+++ src/lib/PopUps/Stake/ConfirmPopUp.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import Popup from '$lib/Common/Popup.svelte';
+	import { convertDate, dateDifference } from '$utils';
+	import { onDestroy } from 'svelte';
+
+	export let amount: number = 15;
+	export let unlockedOn = new Date(2022, 0, 1, 16, 48, 39);
+	export let rewardOn = new Date(2022, 1, 1, 3, 24, 45);
+
+	let realtimeRemaining = dateDifference(rewardOn, new Date());
+
+	let realtimeLoop = setInterval(function () {
+		realtimeRemaining = dateDifference(rewardOn, new Date());
+	}, 1000);
+
+	onDestroy(() => {
+		clearInterval(realtimeLoop);
+	});
+</script>
+
+<Popup title="Are you sure you want to stake?" on:confirm on:cancel hasCancel={true}>
+	<p class="amount">{amount} CSPR</p>
+	<p class="time-until-date">This CSPR will be locked until {convertDate(unlockedOn)}</p>
+	<p class="total-time-until">Total Time Until Reward: {realtimeRemaining}</p>
+</Popup>
+
+<style lang="postcss">
+	p {
+		@apply text-xs mb-1;
+	}
+
+	.amount {
+		@apply text-lg font-bold text-light-appPurple;
+	}
+
+	.time-until-date {
+		@apply font-semibold;
+	}
+
+	.total-time-until {
+		@apply font-bold;
+	}
+</style>
