4c9d8d3f9229fa7847610bc084b64a56c0e5bfd9
Barcleo <barcleokip77@gmail.com>
2022-01-06 10:57:35 +0300

    Refactoring styles to the styles tag

--- src/lib/components/StakingPositionCard/StakingPositionCard.svelte
+++ src/lib/components/StakingPositionCard/StakingPositionCard.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-	export let unstaked: boolean;
-	export let walletName: string;
-	export let stakedAmount: number;
-	export let unlockedAmount: number;
-	export let percentage: number;
-	export let stakedOn: string;
-	export let unstakedOn: string;
-	export let unlockedOn: string;
-	export let rewardOn: string;
-	export let reclaimOn: string;
-	export let timeOne: string;
-	export let timeTwo: string;
-	export let totalTimeUntilReward: string;
+	export let unstaked: boolean = false;
+	export let walletName: string = '';
+	export let stakedAmount: number = 0;
+	export let unlockedAmount: number = 0;
+	export let percentage: number = 0;
+	export let stakedOn: string = '';
+	export let unstakedOn: string = '';
+	export let unlockedOn: string = '';
+	export let rewardOn: string = '';
+	export let reclaimOn: string = '';
+	export let timeOne: string = '';
+	export let timeTwo: string = '';
+	export let totalTimeUntilReward: string = '';
 
 	let startTrigger = 33;
 	let midTrigger = 50;
@@ -21,19 +21,14 @@
 
 <div
 	on:click={() => (selected = true)}
-	class="font-display w-[351px] sm:w-[620px] rounded-[21px] sm:rounded-[22px] bg-white dark:bg-[#2A2F3C] shadow-light-stake-position dark:shadow-dark-stake-position hover:border hover:border-[#725DFF] {selected
-		? 'border border-[#725DFF]'
-		: ''}"
+	class="position-card {selected ? 'border border-[#725DFF]' : ''}"
 >
-	<h2
-		class="font-semibold sm:font-bold text-sm text-black dark:text-white text-left leading-[19px] ml-7 mt-[17px] sm:ml-8 sm:mt-[30px]"
-	>
+	<h2 class="wallet-name">
 		{walletName}
 	</h2>
-	<div class="mt-[9px] flex flex-row items-center justify-between mx-[33px]">
+	<div class="position-details mx-[33px]">
 		<div
-			class="flex flex-col sm:flex-row sm:gap-x-1 justify-center text-xs leading-[15px] text-left {percentage >=
-			startTrigger
+			class="amount-details {percentage >= startTrigger
 				? 'text-[#FF7A28] font-bold'
 				: 'text-[#AEBBC5] font-medium'}"
 		>
@@ -42,29 +37,23 @@
 		</div>
 
 		<div
-			class="flex flex-col sm:flex-row sm:gap-x-1 justify-center text-xs leading-[15px] text-left {percentage >=
-			midTrigger
+			class="amount-details {percentage >= midTrigger
 				? 'text-[#FF8266] font-bold'
 				: 'text-[#AEBBC5] font-medium'}"
 		>
 			<span>{unlockedAmount} CSPR</span>
 			<span>Unlocked</span>
 		</div>
+		<!-- Blank evenly space the amounts -->
 		<div />
 	</div>
-	<div
-		class="mt-[3px] ml-7 mr-[23px] h-[17px] rounded-full bg-neutral-200 dark:bg-black dark:bg-opacity-10"
-	>
-		<div
-			class="h-full rounded-full text-xs leading-none py-1 text-center text-white progress-background"
-			style="width: {percentage}%"
-		/>
+	<div class="progress-bar">
+		<div class="progress progress-background" style="width: {percentage}%" />
 	</div>
-	<div class="mt-[9px] flex flex-row items-center justify-between mx-[23px]">
+	<div class="position-details mx-[23px]">
 		<div
 			class:hidden={unstaked}
-			class="flex flex-col justify-center text-xs leading-[15px] text-center {percentage >=
-			startTrigger
+			class="date-details {percentage >= startTrigger
 				? 'text-[#FF7A28] font-bold'
 				: 'text-[#AEBBC5] font-medium'}"
 		>
@@ -74,8 +63,7 @@
 
 		<div
 			class:hidden={unstaked}
-			class="flex flex-col justify-center text-xs leading-[15px] text-center {percentage >=
-			midTrigger
+			class="date-details {percentage >= midTrigger
 				? 'text-[#FF8266] font-bold'
 				: 'text-[#AEBBC5] font-medium'}"
 		>
@@ -85,8 +73,7 @@
 
 		<div
 			class:hidden={unstaked}
-			class="flex flex-col justify-center text-xs leading-[15px] text-center {percentage >=
-			endTrigger
+			class="date-details {percentage >= endTrigger
 				? 'text-[#FF8266] font-bold'
 				: 'text-[#AEBBC5] font-medium'}"
 		>
@@ -96,8 +83,7 @@
 
 		<div
 			class:hidden={!unstaked}
-			class="flex flex-col justify-center text-xs leading-[15px] text-center {percentage >=
-			startTrigger
+			class="date-details {percentage >= startTrigger
 				? 'text-[#FF7A28] font-bold'
 				: 'text-[#AEBBC5] font-medium'}"
 		>
@@ -107,8 +93,7 @@
 
 		<div
 			class:hidden={!unstaked}
-			class="flex flex-col justify-center text-xs leading-[15px] text-center {percentage >=
-			endTrigger
+			class="date-details {percentage >= endTrigger
 				? 'text-[#FF7A28] font-bold'
 				: 'text-[#AEBBC5] font-medium'}"
 		>
@@ -116,29 +101,50 @@
 			<span>{reclaimOn}</span>
 		</div>
 	</div>
-	<div
-		class="mt-[17px] mx-[23px] flex justify-center space-x-10 text-xs text-neutral-600 dark:text-[#AEBBC5] border-b border-[#4F4F4F] border-dashed divide-x divide-dashed divide-[#4F4F4F]"
-	>
-		<div class="{timeOne === '00:00:00:00' ? 'text-[#FF7A28]' : ''} bg-transparent	">
+	<div class="timers">
+		<div class={timeOne === '00:00:00:00' ? 'text-[#FF7A28]' : ''}>
 			{timeOne}
 		</div>
 		<div class="{timeTwo === '00:00:00:00' ? 'text-[#FF7A28]' : ''} pl-10">
 			{timeTwo}
 		</div>
 	</div>
-	<div class="flex items-center justify-center ">
-		<div
-			class="flex flex-col items-center my-6 text-xs text-[#FF7A28] dark:text-[#AEBBC5] sm:text-[#4F4F4F] text-center"
-		>
-			<span>Total Time Until Reward</span>
-			<span class={totalTimeUntilReward === '00:00:00:00' ? 'text-[#FF7A28]' : ''}>
-				{totalTimeUntilReward}
-			</span>
-		</div>
+	<div class="time-left">
+		<span>Total Time Until Reward</span>
+		<span class={totalTimeUntilReward === '00:00:00:00' ? 'text-[#FF7A28]' : ''}>
+			{totalTimeUntilReward}
+		</span>
 	</div>
 </div>
 
-<style>
+<style lang="postcss" global>
+	:local(.position-card) {
+		@apply font-display w-[351px] sm:w-[620px] rounded-[21px] sm:rounded-[22px] bg-white dark:bg-[#2A2F3C] shadow-light-stake-position dark:shadow-dark-stake-position hover:border hover:border-[#725DFF];
+	}
+	.wallet-name {
+		@apply font-semibold sm:font-bold text-sm text-black dark:text-white text-left leading-[19px] ml-7 mt-[17px] sm:ml-8 sm:mt-[30px];
+	}
+	.position-details {
+		@apply mt-[9px] flex flex-row items-center justify-between;
+	}
+	.amount-details {
+		@apply flex flex-col sm:flex-row sm:gap-x-1 justify-center text-xs leading-[15px] text-left;
+	}
+	.date-details {
+		@apply flex flex-col justify-center text-xs leading-[15px] text-center;
+	}
+	.progress-bar {
+		@apply mt-[3px] ml-7 mr-[23px] h-[17px] rounded-full bg-neutral-200 dark:bg-black dark:bg-opacity-10;
+	}
+	.progress {
+		@apply h-full rounded-full text-xs leading-none py-1 text-center text-white;
+	}
+	.timers {
+		@apply mt-[17px] mx-[23px] flex justify-center space-x-10 text-xs text-neutral-600 dark:text-[#AEBBC5] border-b border-[#4F4F4F] border-dashed divide-x divide-dashed divide-[#4F4F4F];
+	}
+	.time-left {
+		@apply flex flex-col items-center my-6 text-xs text-[#FF7A28] dark:text-[#AEBBC5] sm:text-[#4F4F4F] text-center;
+	}
 	.progress-background {
 		background: linear-gradient(0deg, #ffcb66, #ffcb66), #ffcb66;
 	}
---
+++ src/routes/staking-positions.svelte
@@ -0,0 +1,44 @@
+<script>
+	import StakingPositionCard from '$components/StakingPositionCard/StakingPositionCard.svelte';
+	let stakingCards = [
+		{
+			unstaked: false,
+			walletName: 'Wallet 1',
+			stakedAmount: 15,
+			unlockedAmount: 20,
+			percentage: 33,
+			stakedOn: '1 Dec 2021',
+			unstakedOn: '2 Jan 2022',
+			unlockedOn: '6 Jan 2022',
+			rewardOn: ' 19 Dec 2021',
+			reclaimOn: '21 Dec 2021',
+			timeOne: '00:00:00:00',
+			timeTwo: '19:52:32:23',
+			totalTimeUntilReward: '00:00:00:00',
+		},
+		// {
+		// 	unstaked: true,
+		// 	walletName: 'Wallet 2',
+		// 	stakedAmount: 20,
+		// 	unlockedAmount: 5,
+		// 	percentage: 57,
+		// 	stakedOn: '1 Dec 2021',
+		// 	unstakedOn: '2 Jan 2022',
+		// 	unlockedOn: '6 Jan 2022',
+		// 	rewardOn: ' 19 Dec 2021',
+		// 	reclaimOn: '21 Dec 2021',
+		// 	timeOne: '00:00:00:00',
+		// 	timeTwo: '19:52:32:23',
+		// 	totalTimeUntilReward: '00:00:00:00',
+		// },
+	];
+</script>
+
+<!-- <div class="grid grid-cols-[{stakingCards.length}] gap-10 px-10 py-20 bg-gray-300"> -->
+<div
+	class="inline-grid grid-cols-4 grid-flow-col sm:grid-flow-row gap-10 px-10 py-20 bg-gray-300 overflow-x-scroll"
+>
+	{#each stakingCards as position}
+		<StakingPositionCard {...position} />
+	{/each}
+</div>
