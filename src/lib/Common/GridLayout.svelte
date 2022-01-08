54736c1c90fd923a9c8e0a52c54f81ec53cef211
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-01-08 16:04:21 +0300

    feat: GridLayout component

---
+++ src/lib/Common/GridLayout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import Navbar from '$components/navbar/Navbar.svelte';
+</script>
+
+<div class="app-container">
+	<div class="navbar-container">
+		<Navbar />
+	</div>
+	<div class="left-column">
+		<slot name="left" />
+	</div>
+	<div class="mid-column">
+		<slot name="mid" />
+	</div>
+	<div class="right-column">
+		<slot name="right" />
+	</div>
+</div>
+
+<style lang="postcss">
+	.app-container {
+		@apply w-full grid grid-cols-4 grid-rows-1 gap-0;
+		grid-template-columns: 5vw 25vw auto 25vw;
+	}
+
+	.left-column,
+	.right-column {
+		background-color: red;
+	}
+</style>
