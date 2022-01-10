a71d31a5595f427e662f7af24fbc314635045248
Barcleo <barcleokip77@gmail.com>
2022-01-10 12:05:34 +0300

    Initial scaffolding of the pages

---
+++ src/lib/WalletHome/LandingPage/WalletHead.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import CopyIcon from '$icons/CopyIcon.svelte';
+	import { onMount } from 'svelte';
+
+	export let walletName!: string;
+	export let walletAddress!: string;
+	let displayedWalletAddress!: string;
+	onMount(() => {
+		truncateWalletAddress();
+	});
+	const truncateWalletAddress = () => {
+		displayedWalletAddress =
+			walletAddress.substr(0, 11) +
+			'...' +
+			walletAddress.substr(walletAddress.length - 3, walletAddress.length);
+	};
+	const copyWalletAddress = () => {
+		if (navigator.clipboard) {
+			navigator.clipboard.writeText(walletAddress);
+		}
+	};
+</script>
+
+<!-- TODO remove font family. To be applied globally -->
+<div class="w-full">
+	<!-- Wallet Details -->
+	<div class="flex flex-col md:flex-row items-start text-left">
+		<h2 class="font-bold text-base leading-[22px] text-black">{walletName}</h2>
+		<!-- TODO  SF Pro Display source-->
+		<div class="flex flex-row items-center gap-[11px] text-light-gardenText text-sm font-medium">
+			<p>{displayedWalletAddress}</p>
+			<button type="button" on:click={copyWalletAddress}>
+				<CopyIcon />
+			</button>
+		</div>
+		<h2 class="mt-[19px] font-bold text-black text-base leading-[22px]">Stakes from This Wallet</h2>
+	</div>
+</div>
---
+++ src/routes/wallet-home/index.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import GridLayout from '$lib/Common/GridLayout.svelte';
+</script>
+
+<GridLayout>
+	<div slot="first" class="first">First</div>
+	<div slot="mid" class="bg-gray-300">Scond</div>
+	<div slot="last" class="first">First</div>
+</GridLayout>
