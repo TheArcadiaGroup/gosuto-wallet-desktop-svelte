80a4c9f9b7f02bed602f13a790efb5a4b586047c
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-01-11 21:15:36 +0300

    TextInput darkmode fixes

--- src/lib/Common/TextInput.svelte
+++ src/lib/Common/TextInput.svelte
@@ -13,10 +13,10 @@
 	}
 
 	input {
-		@apply border border-light-gray h-[50px] px-2 w-full rounded-[12px] bg-white dark:bg-dark-background;
+		@apply border border-light-gray dark:border-white dark:border-opacity-30 h-[50px] px-2 w-full rounded-[12px] bg-white dark:bg-dark-background;
 	}
 
 	p {
-		@apply absolute bg-white dark:bg-dark-background transform translate-y-[-7px] translate-x-4 leading-none text-[14px] px-1;
+		@apply absolute bg-white dark:bg-dark-background transform translate-y-[-7px] translate-x-4 leading-none text-[14px] px-1 text-light-grey dark:text-white;
 	}
 </style>
