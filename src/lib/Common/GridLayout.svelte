2f0b2d7f7bbf1c4893a0d9705e4984a94c4ad4b7
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-01-12 14:11:11 +0300

    grid layout background fixes for mobile

--- src/lib/Common/GridLayout.svelte
+++ src/lib/Common/GridLayout.svelte
@@ -17,30 +17,42 @@
 	</div>
 </div>
 
-<style lang="postcss">
-	.app-container {
+<style lang="postcss" global>
+	:local(.app-container) {
 		@apply w-full grid gap-0;
 	}
 
-	.app-container > div {
+	:local(.app-container) > div {
 		@apply overflow-hidden;
 	}
 
 	@media only screen and (max-width: 768px) {
-		.app-container {
+		:local(.app-container) {
 			grid-template-rows: auto auto auto auto;
 		}
 	}
 
 	@media only screen and (min-width: 768px) {
-		.app-container {
+		:local(.app-container) {
 			grid-template-columns: 5vw 25vw auto 25vw;
 		}
 	}
 
-	.first-column,
-	.last-column {
-		@apply z-30;
+	:local(.first-column),
+	:local(.last-column) {
+		@apply z-30 dark:md:bg-dark-blue;
 		box-shadow: 0px 2px 15px rgba(0, 0, 0, 0.05);
+	}
+
+	:local(.first-column) {
+		@apply dark:bg-dark-background;
+	}
+
+	:local(.last-column) {
+		@apply dark:bg-dark-grey;
+	}
+
+	:local(.mid-column) {
+		@apply dark:bg-dark-background;
 	}
 </style>
