e3e547c76510ca4bec95f56101529bfaf9dbda32
Barcleo <barcleokip77@gmail.com>
2022-01-12 07:43:19 +0300

    Responsive to xl and higher screens

--- src/lib/WalletHome/ActionPanel/Unstake.svelte
+++ src/lib/WalletHome/ActionPanel/Unstake.svelte
@@ -45,36 +45,36 @@
 	}
 
 	:local(.header) {
-		@apply hidden md:flex flex-row items-center justify-start gap-x-2.5;
+		@apply hidden md:flex flex-row items-center justify-start gap-x-2.5 xl:gap-x-[1vmax];
 	}
 	.header > button {
-		@apply w-7 h-7 rounded-full hidden md:flex items-center justify-center border-2 border-light-neutrals1;
+		@apply w-7 h-7 xl:w-[2vmax] xl:h-[2vmax] rounded-full hidden md:flex items-center justify-center border-2 border-light-neutrals1;
 	}
 	.header > button > span {
-		@apply w-3 h-3 text-light-neutrals4;
+		@apply w-3 h-3 xl:w-[1vmax] xl:h-[1vmax] text-light-neutrals4;
 	}
 	.header > h2 {
-		@apply font-bold text-[18px] xl:text-3xl leading-[25px] text-center text-black dark:text-white;
+		@apply font-bold text-[18px] xl:text-[1.5vmax] leading-[25px] text-center text-black dark:text-white;
 	}
 	:local(.action-details) {
-		@apply mt-16 md:mt-6 w-full flex flex-col gap-y-4 text-center;
+		@apply mt-16 md:mt-6 xl:mt-[3vmax] w-full flex flex-col gap-y-4 xl:gap-[4.5vmax] text-center;
 	}
 	.action-details > h2 {
-		@apply font-bold text-2xl leading-8 text-black dark:text-white;
+		@apply font-bold text-2xl xl:text-[1.5vmax] leading-8 text-black dark:text-white;
 	}
 	.action-details > p {
-		@apply text-base  leading-6 text-light-grey dark:text-dark-gosutoIconGrey;
+		@apply text-base xl:text-[1.25vmax]  leading-6 text-light-grey dark:text-dark-gosutoIconGrey;
 	}
 	:local(.unstake-wait) {
-		@apply font-normal md:tracking-tighter xl:tracking-normal;
+		@apply font-normal md:tracking-tighter;
 	}
 	:local(.buttons) {
 		@apply flex flex-col items-center w-full;
 	}
 	:local(.action-button) {
-		@apply px-12 py-2.5 md:w-full bg-light-orange rounded-[35px] leading-[26px] text-sm text-white font-bold mt-[75px] md:mt-[30px];
+		@apply px-12 py-2.5 xl:py-[1.25vmax] md:w-full bg-light-orange rounded-full leading-[26px] text-sm md:text-[18px] xl:text-[.8vmax] text-white font-bold mt-[75px] md:mt-[30px] xl:mt-[3vmax];
 	}
 	:local(.cancel-button) {
-		@apply p-0 hidden md:block bg-transparent rounded-[35px] leading-[25px] text-[18px] text-light-grey dark:text-white font-bold mt-6 md:mt-10;
+		@apply p-0 hidden md:block bg-transparent rounded-[35px] leading-[25px] text-[18px] xl:text-[1vmax] text-light-grey dark:text-white font-bold mt-6 md:mt-10 xl:mt-[2.5vmax];
 	}
 </style>
