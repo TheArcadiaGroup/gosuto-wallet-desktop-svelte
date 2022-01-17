ca858dcf8771794bf59b49eda443f441d4fdb2f4
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-01-18 00:37:07 +0300

    refactoring component name

--- src/routes/send.svelte
+++ src/routes/send.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import GridLayout from '$lib/Common/GridLayout.svelte';
-	import Swap from '$lib/Home/Send/index.svelte';
+	import Send from '$lib/Home/Send/index.svelte';
 	import Sidebar from '$lib/Home/Send/sidebar.svelte';
 
 	let sidebarActive: boolean = false;
 </script>
 
 <GridLayout>
-	<Swap
+	<Send
 		slot="mid"
 		on:click={() => {
 			sidebarActive = true;
