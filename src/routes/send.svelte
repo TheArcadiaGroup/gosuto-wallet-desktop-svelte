496fc3afc0e813651f2be8c2b40c61967e1789bf
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-01-18 00:04:11 +0300

    add: send route and reused swap components

---
+++ src/routes/send.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import GridLayout from '$lib/Common/GridLayout.svelte';
+	import Swap from '$lib/Home/Send/index.svelte';
+	import Sidebar from '$lib/Home/Send/sidebar.svelte';
+
+	let sidebarActive: boolean = false;
+</script>
+
+<GridLayout>
+	<Swap
+		slot="mid"
+		on:click={() => {
+			sidebarActive = true;
+		}}
+	/>
+	<Sidebar
+		slot="last"
+		active={sidebarActive}
+		on:closeSidebar={() => {
+			sidebarActive = false;
+		}}
+	/>
+</GridLayout>
