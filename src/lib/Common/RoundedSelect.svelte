4b57b0144e5369c8bf6faf1e759fd0cd92bddd65
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-01-18 03:05:17 +0300

    add: roundedSelect

---
+++ src/lib/Common/RoundedSelect.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let optionsArray: string[] = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
+	export let value: any = null;
+</script>
+
+<div class="select">
+	<select id="standard-select" bind:value>
+		{#each optionsArray as optionString, i}
+			<option value={i}>{optionString}</option>
+		{/each}
+	</select>
+</div>
+
+<style lang="postcss">
+	select {
+		@apply border-0 bg-transparent p-3 m-0 w-full z-10 text-sm;
+	}
+
+	.select {
+		@apply min-w-max w-[20%] cursor-pointer bg-transparent rounded-3xl;
+		@apply border border-light-lighterGray;
+	}
+</style>
