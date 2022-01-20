079eaff171392c4ac1981cfb2d6838ac5688710a
tomashobzaids <thomashobza@ids.company>
2022-01-20 13:20:46 +0100

    add: CreditCard into profile navigation

--- src/lib/components/ProfileNavigation/ProfileNavigation.svelte
+++ src/lib/components/ProfileNavigation/ProfileNavigation.svelte
@@ -1,27 +1,27 @@
 <script>
 	import Button from '$lib/Common/Button.svelte';
+	import ProfilePicture from '$lib/Common/ProfilePicture.svelte';
+	import CreditCard from './CreditCard.svelte';
 </script>
 
-<div class="container bg-white">
+<div class="container ">
 	<div class="flex flex-col items-center gap-6">
-		<div class="profile-pic" />
-		<div class="font-bold">Jake Waterson</div>
+		<div class="w-16 h-16 rounded-xl overflow-hidden">
+			<ProfilePicture url="https://miro.medium.com/fit/c/262/262/2*-cdwKPXyVI0ejgxpWkKBeA.jpeg" />
+		</div>
+		<div class="font-semibold dark:text-white">Jake Waterson</div>
 	</div>
-	<div class="mx-8">
+	<div class="mx-6">
 		<Button>
 			<div slot="text" class="button">+ Add wallet</div>
 		</Button>
 	</div>
+	<CreditCard />
 </div>
 
 <style lang="postcss" global>
 	:local(.container) {
-		@apply w-full h-full flex flex-col items-stretch gap-7 pt-20;
-	}
-
-	:local(.profile-pic) {
-		background-image: url('https://i.pinimg.com/originals/bf/57/02/bf57026ee75af2f414000cec322f7404.gif');
-		@apply w-20 h-20 bg-cover rounded-xl dark:text-white shadow-inner;
+		@apply w-full h-full flex flex-col items-stretch gap-7 pt-20 px-4;
 	}
 
 	:local(.button) {
