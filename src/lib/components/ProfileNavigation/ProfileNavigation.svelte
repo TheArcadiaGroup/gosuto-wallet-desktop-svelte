cdb18e4a856034643a7960a41c1752a646d04edc
tomashobzaids <thomashobza@ids.company>
2022-01-20 15:43:09 +0100

    add: ProfileMenu into profile navigation

--- src/lib/components/ProfileNavigation/ProfileNavigation.svelte
+++ src/lib/components/ProfileNavigation/ProfileNavigation.svelte
@@ -3,29 +3,37 @@
 	import ProfilePicture from '$lib/Common/ProfilePicture.svelte';
 	import CreditCard from './CreditCard.svelte';
 	import NavigationArrows from './NavigationArrows.svelte';
+	import ProfileMenu from './ProfileMenu.svelte';
 </script>
 
-<div class="container ">
+<div class="container">
 	<div class="flex flex-col items-center gap-6">
 		<div class="w-16 h-16 rounded-xl overflow-hidden">
 			<ProfilePicture url="https://miro.medium.com/fit/c/262/262/2*-cdwKPXyVI0ejgxpWkKBeA.jpeg" />
 		</div>
 		<div class="font-semibold dark:text-white">Jake Waterson</div>
 	</div>
+
 	<div class="mx-6">
 		<Button>
 			<div slot="text" class="button">+ Add wallet</div>
 		</Button>
 	</div>
+
 	<CreditCard />
+
 	<div class="h-6">
 		<NavigationArrows />
 	</div>
+
+	<hr />
+
+	<ProfileMenu />
 </div>
 
 <style lang="postcss" global>
 	:local(.container) {
-		@apply w-full h-full flex flex-col items-stretch gap-7 pt-20 px-4;
+		@apply w-full h-full flex flex-col items-stretch gap-6 px-4 overflow-auto py-10;
 	}
 
 	:local(.button) {
