473cac7e337975d19b0e8053a052e8b483d1acf9
tomashobzaids <thomashobza@ids.company>
2022-01-20 15:27:02 +0100

    add: NavigationArrows component

---
+++ src/lib/components/ProfileNavigation/NavigationArrows.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import ArrowInCircle from '$lib/Common/ArrowInCircle.svelte';
+
+	function moveWallets(e: CustomEvent) {
+		// console.log(e.detail.direction);
+	}
+</script>
+
+<div class="w-full h-full flex flex-row items-center justify-center gap-2">
+	<ArrowInCircle direction="left" disabled={true} on:click={moveWallets} />
+	<ArrowInCircle direction="right" disabled={false} on:click={moveWallets} />
+</div>
