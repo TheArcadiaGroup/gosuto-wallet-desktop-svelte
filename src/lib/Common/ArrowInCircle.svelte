b4bab5bc239ae81fd0e9e0eaa726a342520c7a7b
tomashobzaids <thomashobza@ids.company>
2022-01-20 15:24:30 +0100

    add: ArrowInCircle component

---
+++ src/lib/Common/ArrowInCircle.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	import ArrowLeft from '$icons/ArrowLeft.svelte';
+
+	export let direction: 'left' | 'right' = 'left';
+	export let disabled = true;
+
+	function click() {
+		if (disabled) return;
+
+		dispatch('click', { direction });
+	}
+</script>
+
+<div
+	class="h-full aspect-square rounded-full border-transparent border-2 grid place-items-center dark:text-white transition-all {!disabled &&
+		'cursor-pointer dark:hover:border-white hover:border-light-gray'}"
+	on:click={click}
+>
+	<div class="w-1/2 {direction == 'right' && 'rotate-180'}">
+		<ArrowLeft />
+	</div>
+</div>
