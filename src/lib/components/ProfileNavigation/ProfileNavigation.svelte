0decbda6833cf9f95eb6fc0fa2a4baef73fbc0f3
tomashobzaids <thomashobza@ids.company>
2022-01-21 15:53:26 +0100

    add: props in ProfileNavigation component

--- src/lib/components/ProfileNavigation/ProfileNavigation.svelte
+++ src/lib/components/ProfileNavigation/ProfileNavigation.svelte
@@ -4,14 +4,26 @@
 	import CreditCard from './CreditCard.svelte';
 	import NavigationArrows from './NavigationArrows.svelte';
 	import ProfileMenu from './ProfileMenu.svelte';
+
+	export let name = 'Jake Waterson';
+	export let ppurl = 'https://miro.medium.com/fit/c/262/262/2*-cdwKPXyVI0ejgxpWkKBeA.jpeg';
+	export let avalible = 5000;
+	export let staked = 2500;
+	export let unclaimed = 375;
+
+	function moveCardCarousel(e) {
+		const direction = e.detail.direction;
+	}
 </script>
 
 <div class="container">
 	<div class="flex flex-col items-center gap-6">
 		<div class="w-16 h-16 rounded-xl overflow-hidden">
-			<ProfilePicture url="https://miro.medium.com/fit/c/262/262/2*-cdwKPXyVI0ejgxpWkKBeA.jpeg" />
+			<ProfilePicture url={ppurl} />
 		</div>
-		<div class="font-semibold dark:text-white">Jake Waterson</div>
+		<div class="font-semibold dark:text-white">
+			{name}
+		</div>
 	</div>
 
 	<div class="mx-6">
@@ -20,10 +32,10 @@
 		</Button>
 	</div>
 
-	<CreditCard />
+	<CreditCard {name} {ppurl} {avalible} {staked} {unclaimed} />
 
 	<div class="h-6">
-		<NavigationArrows />
+		<NavigationArrows on:move={moveCardCarousel} />
 	</div>
 
 	<hr />
