4a391b793b2aba6c3e7c12b042ae3a75b8e08ee8
tomashobzaids <thomashobza@ids.company>
2022-01-21 18:47:41 +0100

    add: mobile UI for profile navigation

--- src/lib/components/ProfileNavigation/ProfileNavigation.svelte
+++ src/lib/components/ProfileNavigation/ProfileNavigation.svelte
@@ -1,4 +1,6 @@
 <script>
+	import PlusIcon from '$icons/PlusIcon.svelte';
+
 	import Button from '$lib/Common/Button.svelte';
 	import ProfilePicture from '$lib/Common/ProfilePicture.svelte';
 	import CreditCard from './CreditCard.svelte';
@@ -14,10 +16,14 @@
 	function moveCardCarousel(e) {
 		const direction = e.detail.direction;
 	}
+
+	function menuSelect(e) {
+		const selection = e.detail.menu_item;
+	}
 </script>
 
 <div class="container">
-	<div class="flex flex-col items-center gap-6">
+	<div class="flex-col items-center gap-6 hidden md:flex">
 		<div class="w-16 h-16 rounded-xl overflow-hidden">
 			<ProfilePicture url={ppurl} />
 		</div>
@@ -26,29 +32,46 @@
 		</div>
 	</div>
 
-	<div class="mx-6">
+	<div class="mx-6 max-w-xs w-full not-on-mobile">
 		<Button>
 			<div slot="text" class="button">+ Add wallet</div>
 		</Button>
 	</div>
 
-	<CreditCard {name} {ppurl} {avalible} {staked} {unclaimed} />
+	<div class="w-full flex items-center justify-center relative">
+		<CreditCard {name} {ppurl} {avalible} {staked} {unclaimed} />
+		<div class="absolute h-full aspect-[16/10] pointer-events-none">
+			<div
+				class="absolute z-10 w-12 h-12 -right-3 bg-light-orange grid place-items-center -bottom-3 md:hidden rounded-full pointer-events-auto cursor-pointer transition-all hover:scale-105"
+			>
+				<PlusIcon />
+			</div>
+		</div>
+	</div>
 
-	<div class="h-6">
+	<div class="h-6 w-full max-w-xs not-on-mobile">
 		<NavigationArrows on:move={moveCardCarousel} />
 	</div>
 
-	<hr />
+	<div class="w-full not-on-mobile">
+		<hr />
+	</div>
 
-	<ProfileMenu />
+	<div class="w-full md:max-w-xs">
+		<ProfileMenu on:click={menuSelect} />
+	</div>
 </div>
 
 <style lang="postcss" global>
 	:local(.container) {
-		@apply w-full h-full flex flex-col items-stretch gap-6 px-4 overflow-auto py-10;
+		@apply w-full h-full flex flex-col items-center gap-6 md:px-4 overflow-auto py-10;
 	}
 
 	:local(.button) {
 		@apply my-2 text-xs;
+	}
+
+	:local(.not-on-mobile) {
+		@apply hidden md:block;
 	}
 </style>
