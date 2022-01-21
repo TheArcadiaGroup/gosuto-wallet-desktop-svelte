efb629d8d9b781c4365f7a58b95a70552f9a629a
tomashobzaids <thomashobza@ids.company>
2022-01-21 19:54:54 +0100

    add: navigation arrows functionality

--- src/lib/components/ProfileNavigation/NavigationArrows.svelte
+++ src/lib/components/ProfileNavigation/NavigationArrows.svelte
@@ -3,12 +3,19 @@
 	import { createEventDispatcher } from 'svelte';
 	const dispatch = createEventDispatcher();
 
+	export let carouselPosition = 0;
+	export let numberOfCards: number;
+
 	function moveWallets(e: CustomEvent) {
 		dispatch('move', { direction: e.detail.direction === 'right' ? 1 : -1 });
 	}
 </script>
 
 <div class="w-full h-full flex flex-row items-center justify-center gap-2">
-	<ArrowInCircle direction="left" disabled={true} on:click={moveWallets} />
-	<ArrowInCircle direction="right" disabled={false} on:click={moveWallets} />
+	<ArrowInCircle direction="left" disabled={!(carouselPosition > 0)} on:click={moveWallets} />
+	<ArrowInCircle
+		direction="right"
+		disabled={!(carouselPosition < numberOfCards - 1)}
+		on:click={moveWallets}
+	/>
 </div>
