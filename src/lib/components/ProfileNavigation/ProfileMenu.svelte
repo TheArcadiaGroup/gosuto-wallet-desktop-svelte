bbb90efb6feb999bac3ceec321790bac9bf73419
tomashobzaids <thomashobza@ids.company>
2022-01-21 18:47:18 +0100

    add: ProfileMenu click events

--- src/lib/components/ProfileNavigation/ProfileMenu.svelte
+++ src/lib/components/ProfileNavigation/ProfileMenu.svelte
@@ -4,43 +4,73 @@
 	import Stake from '$icons/Stake.svelte';
 	import Swap from '$icons/Swap.svelte';
 	import Wallet from '$icons/Wallet.svelte';
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	function click(menu_item: 'history' | 'send' | 'stake' | 'settings' | 'swap') {
+		dispatch('click', { menu_item });
+	}
 </script>
 
-<div class="w-full flex flex-col items-stretch gap-2">
-	<div class="menu-item">
-		<div class="w-3">
+<div
+	class="no-scrollbar py-3 px-6 md:px-0 md:py-0 w-full flex flex-row overflow-x-auto md:flex-col items-stretch gap-2"
+>
+	<div class="menu-item" on:click={() => click('history')}>
+		<div class="icon">
 			<Bookmark />
 		</div>
-		History
+		<div class="label">History</div>
 	</div>
-	<div class="menu-item">
-		<div class="w-3">
+	<div class="menu-item" on:click={() => click('send')}>
+		<div class="icon">
 			<PaperAirplane />
 		</div>
-		Send
+		<div class="label">Send</div>
 	</div>
-	<div class="menu-item">
-		<div class="w-3">
+	<div class="menu-item" on:click={() => click('stake')}>
+		<div class="icon">
 			<Stake />
 		</div>
-		Stake
+		<div class="label">Stake</div>
 	</div>
-	<div class="menu-item">
-		<div class="w-3">
+	<div class="menu-item" on:click={() => click('settings')}>
+		<div class="icon">
 			<Wallet />
 		</div>
-		Wallet Settings
+		<div class="label">Wallet Settings</div>
 	</div>
-	<div class="menu-item">
-		<div class="w-3">
+	<div class="menu-item" on:click={() => click('swap')}>
+		<div class="icon">
 			<Swap />
 		</div>
-		Swap
+		<div class="label">Swap</div>
 	</div>
 </div>
 
 <style lang="postcss" global>
 	:local(.menu-item) {
-		@apply rounded-lg hover:bg-light-purple hover:shadow transition-all px-4 py-3 text-xs text-light-grey hover:text-white dark:text-white cursor-pointer flex flex-row items-center gap-2;
+		@apply rounded-lg hover:bg-light-purple hover:shadow-md transition-all px-4 py-3 text-xs text-light-grey hover:text-white dark:text-white cursor-pointer flex flex-row items-center gap-2 shadow-design md:shadow-none md:hover:shadow;
+		outline: none;
+	}
+
+	:local(.menu-item):focus {
+		outline: none;
+	}
+
+	:local(.no-scrollbar) {
+		-ms-overflow-style: none; /* IE and Edge */
+		scrollbar-width: none; /* Firefox */
+	}
+
+	:local(.no-scrollbar)::-webkit-scrollbar {
+		display: none;
+	}
+
+	:local(.icon) {
+		@apply w-4;
+	}
+
+	:local(.label) {
+		@apply whitespace-nowrap;
 	}
 </style>
