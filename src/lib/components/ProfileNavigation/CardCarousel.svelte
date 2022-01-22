eb2d547d11484d470e31538e011976419e2f5a66
tomashobzaids <thomashobza@ids.company>
2022-01-22 18:31:39 +0100

    add: swiping to card carousel

--- src/lib/components/ProfileNavigation/CardCarousel.svelte
+++ src/lib/components/ProfileNavigation/CardCarousel.svelte
@@ -1,17 +1,40 @@
 <script>
+	import { swipe } from 'svelte-gestures';
+	import NavigationArrows from './NavigationArrows.svelte';
+	let direction;
+	let target;
+
 	export let position = 0;
+	export let numberOfCards = 1; // TODO this will be later the length of an array with cards
+
+	function handler(e) {
+		e.detail.direction = e.detail.direction === 'right' ? -1 : 1;
+		moveCardCarousel(e);
+	}
+
+	function moveCardCarousel(e) {
+		const direction = e.detail.direction;
+		if (position + direction >= 0 && position + direction < numberOfCards) {
+			position += direction;
+		}
+	}
+
+	$: console.log('position', position);
 </script>
 
 <div
-	class="w-full flex flex-row overflow-x-scroll md:overflow-x-visible snap-x px-6 md:px-1 md:gap-2 no-scrollbar py-2 transition-all carousel"
+	class="w-full flex flex-row overflow-x-visible snap-x md:px-1 md:gap-2 no-scrollbar py-2 transition-all duration-1000 carousel"
 	style="--scroll: translateX({-position * 100}%);"
+	use:swipe={{ timeframe: 300, minSwipeDistance: 60 }}
+	on:swipe={handler}
 >
 	<div class="w-full flex-shrink-0 flex items-center justify-center snap-center">
 		<slot />
 	</div>
-	<div class="w-full flex-shrink-0 flex items-center justify-center snap-center">
-		<slot />
-	</div>
+</div>
+
+<div class="h-6 w-full mt-4 not-on-mobile">
+	<NavigationArrows on:move={moveCardCarousel} carouselPosition={position} {numberOfCards} />
 </div>
 
 <style lang="postcss" global>
@@ -26,5 +49,9 @@
 
 	:local(.carousel) {
 		transform: var(--scroll);
+	}
+
+	:local(.not-on-mobile) {
+		@apply hidden md:block;
 	}
 </style>
