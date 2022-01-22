cd650aad68bae4eab917308c1bafca006142caab
tomashobzaids <thomashobza@ids.company>
2022-01-23 00:24:07 +0100

    add: ProgressBar component

---
+++ src/lib/Common/ProgressBar.svelte
@@ -0,0 +1,7 @@
+<script>
+	export let value = 0;
+</script>
+
+<div class="w-full h-full bg-light-scrollBar rounded-full overflow-hidden">
+	<div class="h-full rounded-full bg-light-orangeShadeOne transition-all" style="width: {value}%" />
+</div>
