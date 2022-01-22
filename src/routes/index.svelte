54cf009357daf4101191b3eeba97381d1e3de4f9
tomashobzaids <thomashobza@ids.company>
2022-01-23 00:26:40 +0100

    add: StakesFromWallet to slot in index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,11 @@
 <script>
 	import GridLayout from '$lib/Common/GridLayout.svelte';
+	import StakesFromWallet from '$lib/components/StakesFromWallet/StakesFromWallet.svelte';
 </script>
 
-<GridLayout />
+<GridLayout>
+	<div slot="first" class="w-full h-full bg-light-gray">llulw</div>
+	<div slot="mid" class="w-full h-full">
+		<StakesFromWallet />
+	</div>
+</GridLayout>
