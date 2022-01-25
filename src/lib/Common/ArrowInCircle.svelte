f145cf2e5f29a2cb5e3b5947f22e514bb9d4cf18
tomashobzaids <thomashobza@ids.company>
2022-01-26 00:16:27 +0100

    add: always showing border option to ArrowInCircle

--- src/lib/Common/ArrowInCircle.svelte
+++ src/lib/Common/ArrowInCircle.svelte
@@ -2,15 +2,18 @@
 	import { createEventDispatcher } from 'svelte';
 	const dispatch = createEventDispatcher();
 	import ArrowLeft from '$icons/ArrowLeft.svelte';
+
 	export let direction: 'left' | 'right' = 'left';
 	export let disabled = true;
+	export let alwaysShowBorder = false;
+
 	function click() {
 		if (disabled) return;
 		dispatch('click', { direction });
 	}
 </script>
 
-<div class="{!disabled ? 'disabled' : ''} main" on:click={click}>
+<div class="{!disabled ? 'not-disabled' : ''} main {alwaysShowBorder && "show-border"}" on:click={click}>
 	<div class="w-1/2 {direction == 'right' && 'rotate-180'}">
 		<ArrowLeft />
 	</div>
@@ -20,7 +23,12 @@
 	:local(.main) {
 		@apply h-full aspect-square rounded-full border-transparent border-2 grid place-items-center dark:text-white transition-all;
 	}
-	:local(.disabled) {
+	
+	:local(.not-disabled) {
 		@apply cursor-pointer dark:hover:border-white hover:border-light-gray;
+	}
+
+	:local(.show-border) {
+		@apply dark:border-white border-light-gray;
 	}
 </style>
