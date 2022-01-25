2713d1fb79c21ac24ab98a4edb8580073b87206f
tomashobzaids <thomashobza@ids.company>
2022-01-26 00:20:49 +0100

    add: correct subrouting

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,5 @@
 <script>
 	import GridLayout from '$lib/Common/GridLayout.svelte';
-	import StakesFromWallet from '$lib/components/StakesFromWallet/StakesFromWallet.svelte';
 </script>
 
-<GridLayout>
-	<div slot="first" class="w-full h-full bg-light-gray">llulw</div>
-	<div slot="mid" class="w-full h-full">
-		<StakesFromWallet />
-	</div>
-</GridLayout>
+<GridLayout />
---
+++ src/routes/profile/stakes/[slug].svelte
@@ -0,0 +1,28 @@
+<script>
+	import { page } from '$app/stores';
+	import GridLayout from '$lib/Common/GridLayout.svelte';
+	import StakesFromWallet from '$lib/components/StakesFromWallet/StakesFromWallet.svelte';
+
+	// wallet bind on profile navigation component
+
+	$: slug = $page.params.slug;
+</script>
+
+<GridLayout>
+	<div slot="first">
+		<!-- Profile Navigation Component with highlighted "stakes" menu option -->
+	</div>
+	<div slot="mid" class="w-full h-full">
+		<!-- passing wallet to stakes component -->
+		<StakesFromWallet />
+	</div>
+	<div slot="last" class="last-column">
+		<!-- stake info component -->
+	</div>
+</GridLayout>
+
+<style lang="postcss" global>
+	:local(.last-column) {
+		@apply w-full h-full grid place-content-center text-light-grey text-xs;
+	}
+</style>
