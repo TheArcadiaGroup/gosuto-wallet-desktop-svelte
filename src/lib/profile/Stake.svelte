98900d0ddad59e0d215eaf2d0a9d16939e0a4b56
tomashobzaids <thomashobza@ids.company>
2022-01-27 15:48:14 +0100

    add: structure for /profile/stakes

---
+++ src/lib/profile/Stake.svelte
@@ -0,0 +1,35 @@
+<script>
+	import GridLayout from '$lib/Common/GridLayout.svelte';
+	import ProfileNavigation from '$lib/profile/ProfileNavigation.svelte';
+	import Confirm from '$lib/profile/stake/detail/Confirm.svelte';
+	import { page } from '$app/stores';
+
+	$: slug = $page.params.slug;
+	// Show a specific profile page based on slug
+
+	// wallet bind on profile navigation component
+</script>
+
+<GridLayout>
+	<div slot="first" class="size-full">
+		<ProfileNavigation />
+	</div>
+	<div slot="mid" class="w-full h-full">StakesFromWallet component</div>
+	<div slot="last" class="last-column">
+		{#if false}
+			Select a stake for more information
+		{:else}
+			<Confirm />
+		{/if}
+	</div>
+</GridLayout>
+
+<style lang="postcss" global>
+	:local(.last-column) {
+		@apply w-full h-full grid place-content-center text-light-grey text-xs;
+	}
+
+	:local(.size-full) {
+		@apply w-full h-full;
+	}
+</style>
