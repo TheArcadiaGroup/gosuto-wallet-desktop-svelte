e7cdac2196f8e16e47025a838299219e19c4a2f7
tomashobzaids <thomashobza@ids.company>
2022-01-31 00:51:08 +0100

    change: redo menu and add current route highlight

--- src/lib/profile/ProfileMenu.svelte
+++ src/lib/profile/ProfileMenu.svelte
@@ -4,45 +4,38 @@
 	import Stake from '$icons/Stake.svelte';
 	import Swap from '$icons/Swap.svelte';
 	import Wallet from '$icons/Wallet.svelte';
+
+	import { page } from '$app/stores';
+	$: currentSubroute = $page.path.split('/')[2] || '';
+
 	import { createEventDispatcher } from 'svelte';
 	const dispatch = createEventDispatcher();
 
-	function click(menu_item: 'history' | 'send' | 'stake' | 'settings' | 'swap') {
+	function click(menu_item: string) {
 		dispatch('click', { menu_item });
 	}
+
+	const menuItems = [
+		{ name: 'history', label: 'History', icon: Bookmark },
+		{ name: 'send', label: 'Send', icon: PaperAirplane },
+		{ name: 'stakes', label: 'Stakes', icon: Stake },
+		{ name: 'settings', label: 'Wallet Settings', icon: Wallet },
+		{ name: 'swap', label: 'Swap', icon: Swap },
+	];
 </script>
 
 <div class="no-scrollbar main">
-	<div class="menu-item" on:click={() => click('history')}>
-		<div class="icon">
-			<Bookmark />
+	{#each menuItems as item}
+		<div
+			class="menu-item {currentSubroute === item.name && 'currently-open'}"
+			on:click={() => click(item.name || 'unknown')}
+		>
+			<div class="icon">
+				<svelte:component this={item.icon} />
+			</div>
+			<div class="label">{item.label || 'unknown'}</div>
 		</div>
-		<div class="label">History</div>
-	</div>
-	<div class="menu-item" on:click={() => click('send')}>
-		<div class="icon">
-			<PaperAirplane />
-		</div>
-		<div class="label">Send</div>
-	</div>
-	<div class="menu-item" on:click={() => click('stake')}>
-		<div class="icon">
-			<Stake />
-		</div>
-		<div class="label">Stake</div>
-	</div>
-	<div class="menu-item" on:click={() => click('settings')}>
-		<div class="icon">
-			<Wallet />
-		</div>
-		<div class="label">Wallet Settings</div>
-	</div>
-	<div class="menu-item" on:click={() => click('swap')}>
-		<div class="icon">
-			<Swap />
-		</div>
-		<div class="label">Swap</div>
-	</div>
+	{/each}
 </div>
 
 <style lang="postcss" global>
@@ -53,6 +46,10 @@
 	:local(.menu-item) {
 		@apply rounded-lg hover:bg-light-purple hover:shadow-md transition-all px-4 py-3 text-xs text-light-grey hover:text-white dark:text-white cursor-pointer flex flex-row items-center gap-2 shadow-design md:shadow-none md:hover:shadow;
 		outline: none;
+	}
+
+	:local(.currently-open) {
+		@apply bg-light-purple;
 	}
 
 	:local(.menu-item):focus {
