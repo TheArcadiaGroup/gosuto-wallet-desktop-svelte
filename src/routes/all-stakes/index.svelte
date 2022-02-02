0e2ef803d827401d5236d5aa177dcf0661a438d4
tomashobzaids <thomashobza@ids.company>
2022-02-02 16:08:39 +0100

    add: add-stakes route index file

---
+++ src/routes/all-stakes/index.svelte
@@ -0,0 +1,37 @@
+<script>
+	import GridLayout from '$lib/Common/GridLayout.svelte';
+	import ProfileNavigation from '$lib/profile/ProfileNavigation.svelte';
+
+	// DEV
+	const user = {
+		name: 'Jake Waterson',
+		ppurl: 'https://miro.medium.com/fit/c/262/262/2*-cdwKPXyVI0ejgxpWkKBeA.jpeg',
+		wallets: [
+			{
+				name: 'Wallet 1',
+				avalible: 5000,
+				staked: 2500,
+				unclaimed: 375,
+			},
+			{
+				name: 'Wallet 1',
+				avalible: 5000,
+				staked: 2500,
+				unclaimed: 375,
+			},
+		],
+	};
+</script>
+
+<GridLayout>
+	<div slot="first" class="size-full">
+		<!-- feed the user profile data to ProfileNavigation component -->
+		<ProfileNavigation forRoute={'all-stakes'} {user} />
+	</div>
+</GridLayout>
+
+<style lang="postcss" global>
+	:local(.size-full) {
+		@apply w-full h-full;
+	}
+</style>
