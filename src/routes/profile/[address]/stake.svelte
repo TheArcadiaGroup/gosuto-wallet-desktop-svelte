feac9cf332371299525e4390ef746a742406cde2
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-02-08 22:06:25 +0300

    route changes for the stake and profile/stake

--- src/routes/profile/[address]/stake.svelte
+++ src/routes/profile/[address]/stake.svelte
@@ -1,5 +1,43 @@
-<script>
-	import StakePage from '$lib/Profile/stake/StakePage.svelte';
+<script context="module">
+	export const load = async ({ _, page }) => {
+		const address = page.params.address;
+		return {
+			props: {
+				address,
+			},
+		};
+	};
 </script>
 
-<StakePage />
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	import StakePage from '$lib/Profile/stake/StakePage.svelte';
+
+	import { shortenAddress } from '$utils';
+
+	let data;
+
+	export let address: string;
+
+	onMount(() => {
+		getData(address);
+	});
+
+	const getData = async (address: string) => {
+		fetch(`../../api/profile/[address]/stake.json/?address=${address}`)
+			.then((response) => response.json())
+			.then((response) => (data = response))
+			.catch((error) => {
+				console.error('error:', error);
+			});
+	};
+</script>
+
+<StakePage
+	stakeArray={data}
+	wallet={{
+		name: 'Wallet 2',
+		publicKey: address,
+	}}
+/>
