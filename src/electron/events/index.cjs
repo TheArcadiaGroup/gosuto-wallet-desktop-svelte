c0ad7c03286f8279ec2e840964065a0722a487ad
1nsane-dev <1nsane.dev01@gmail.com>
2022-02-22 13:36:29 -0500

    get transfer history

--- src/electron/events/index.cjs
+++ src/electron/events/index.cjs
@@ -1,5 +1,6 @@
 const { ipcMain } = require('electron');
 const createWallet = require('../utils/createWallet.cjs');
+const profileHistory = require('../utils/profileHistory.cjs');
 const sendMessage = require('./sendMessage.cjs');
 
 /**
@@ -12,7 +13,10 @@ module.exports = () => {
 	});
 
 	ipcMain.on('createWallet', async (event, data) => {
-		console.log('Please create wallet', data);
+		const res = await profileHistory.getTransferHistory({
+			accountHash: '34b0394b11dc3ecb1bf6f26c9754aa2e9f38d7bec33003374b4b3fac8566c258',
+		});
+		console.log('Please create wallet', res);
 
 		// return this as an event to the renderer process
 		sendMessage('createWalletResponse', createWallet());
