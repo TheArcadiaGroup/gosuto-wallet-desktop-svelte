d223e719641a5b02f044c929c8e360d202d9d93e
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-03-06 03:35:09 +0300

    generator cleanup

--- src/routes/api/profile/[address]/stake/index.ts
+++ src/routes/api/profile/[address]/stake/index.ts
@@ -1,40 +1,8 @@
-//DEV
-const wallet = {
-	name: 'wallet-1',
-	publicKey: '0x9f98e01d2fn89hc9cgqc24ed7',
-};
-let stakeArray: IStake[] = Array(10).fill({
-	parentWallet: wallet.name,
-	stakeAmount: 420,
-	unstakeDatetime: new Date(2021, 11, 17),
-	unstakeCountdown: Math.abs(new Date(2020, 12, 17) - new Date(2019, 11, 17)),
-	reclamationDate: new Date(2020, 12, 17),
-	initialStakeDate: new Date(2019, 11, 17),
-	rewardDate: new Date(2022, 11, 17),
-	rewardCountdown: Math.abs(new Date(2022, 11, 17) - new Date(2019, 11, 17)),
-	reward: 420,
-	unlocked: 84,
-	stakePercent: 0.8,
-	parentWalletAddress: wallet.publicKey,
-});
-stakeArray.push({
-	parentWallet: 'wallet-2',
-	stakeAmount: 420,
-	unstakeDatetime: new Date(2021, 11, 17),
-	unstakeCountdown: Math.abs(new Date(2020, 12, 17) - new Date(2019, 11, 17)),
-	reclamationDate: new Date(2020, 12, 17),
-	initialStakeDate: new Date(2019, 11, 17),
-	rewardDate: new Date(2022, 11, 17),
-	rewardCountdown: Math.abs(new Date(2022, 11, 17) - new Date(2019, 11, 17)),
-	reward: 420,
-	unlocked: 84,
-	stakePercent: 0.8,
-	parentWalletAddress: wallet.publicKey,
-});
+import { generateStakes } from '$utils/stakeDataSample';
 
 export async function post(query): Promise<any> {
 	let address = await query.body;
-	let transactions = stakeArray;
+	let transactions = generateStakes();
 	//dev//
 	let data = transactions.filter((transaction) => transaction.parentWallet == address);
 	return {
