0aa82f4041e73adc03ff00de1cb881bce0e2752f
1nsane-dev <1nsane.dev01@gmail.com>
2022-03-17 08:30:09 -0400

    get validators && delegate

--- src/electron/events/index.cjs
+++ src/electron/events/index.cjs
@@ -1,6 +1,8 @@
 const { ipcMain } = require('electron');
 const createWallet = require('../utils/createWallet.cjs');
+const staking = require('../utils/staking.cjs');
 const sendMessage = require('./sendMessage.cjs');
+const { Keys } = require('casper-js-sdk');
 
 /**
  * Receiving messages from Renderer
@@ -12,9 +14,18 @@ module.exports = () => {
 	});
 
 	ipcMain.on('createWallet', async (event, data) => {
-		console.log('Please create wallet', data);
+		const res = await staking.delegate({
+			privateKey: Keys.Ed25519.parsePrivateKey(
+				Keys.Ed25519.readBase64WithPEM(
+					'-----BEGIN PRIVATE KEY-----\r\nMC4CAQAwBQYDK2VwBCIEIFvkdWUFtcpt2yOrbWk+v1fHf0y3Ca3+idJYXGkPKV+y\r\n-----END PRIVATE KEY-----\r\n',
+				),
+			),
+			validatorPublicKey: '0106ca7c39cd272dbf21a86eeb3b36b7c26e2e9b94af64292419f7862936bca2ca',
+			amount: 10,
+		});
+		console.log('test delegate', res);
 
 		// return this as an event to the renderer process
-		sendMessage('createWalletResponse', createWallet());
+		sendMessage('createWalletResponse', res);
 	});
 };
---
+++ src/electron/utils/staking.cjs
@@ -0,0 +1,77 @@
+const axios = require('axios');
+const {
+	Keys,
+	CasperClient,
+	CLPublicKey,
+	DeployUtil,
+	RuntimeArgs,
+	CLValue,
+	CLValueBuilder,
+	CLU512,
+} = require('casper-js-sdk');
+const { casperDelegationContractHexCode } = './constants';
+
+const TEST_NET_RPC_API = 'http://52.70.214.247:7777/rpc';
+const MAIN_NET_RPC_API = 'http://52.70.214.247:7777/rpc';
+
+module.exports = {
+	// Get all validator info
+	getAllValidators: async () => {
+		const res = await axios.get('https://api.caspercommunity.io/validators');
+		return res.data;
+	},
+
+	// Delegate stake
+	delegate: async ({ privateKey, validatorPublicKey, amount, network = 'casper-test' }) => {
+		const client = new CasperClient(network === 'casper' ? MAIN_NET_RPC_API : TEST_NET_RPC_API);
+		const publicKey = Keys.Ed25519.privateToPublicKey(privateKey);
+		const keyPair = Keys.Ed25519.parseKeyPair(publicKey, privateKey);
+		const deployParams = new DeployUtil.DeployParams(keyPair.publicKey, network);
+		const payment = DeployUtil.standardPayment(5000000000);
+		const args = RuntimeArgs.fromMap({
+			delegator: keyPair.publicKey,
+			validator: CLPublicKey.fromHex(validatorPublicKey),
+			amount: CLValueBuilder.u512(amount),
+		});
+		let contractHash = 'ccb576d6ce6dec84a551e48f0d0b7af89ddba44c7390b690036257a04a3ae9ea';
+		if (network === 'casper-test') {
+			contractHash = '93d923e336b20a4c4ca14d592b60e5bd3fe330775618290104f9beb326db7ae2';
+		}
+		const session = DeployUtil.ExecutableDeployItem.newStoredContractByHash(
+			Uint8Array.from(Buffer.from(contractHash, 'hex')),
+			'delegate',
+			args,
+		);
+		const deploy = DeployUtil.makeDeploy(deployParams, session, payment);
+		const signedDeploy = DeployUtil.signDeploy(deploy, keyPair);
+		const executionResult = await client.putDeploy(signedDeploy);
+		return executionResult;
+	},
+
+	// Undelegate
+	undelegate: async ({ privateKey, validatorPublicKey, amount, network = 'casper-test' }) => {
+		const client = new CasperClient(network === 'casper' ? MAIN_NET_RPC_API : TEST_NET_RPC_API);
+		const publicKey = Keys.Ed25519.privateToPublicKey(privateKey);
+		const keyPair = Keys.Ed25519.parseKeyPair(publicKey, privateKey);
+		const deployParams = new DeployUtil.DeployParams(keyPair.publicKey, network);
+		const payment = DeployUtil.standardPayment(5000000000);
+		const args = RuntimeArgs.fromMap({
+			delegator: keyPair.publicKey,
+			validator: CLPublicKey.fromHex(validatorPublicKey),
+			amount: CLValueBuilder.u512(amount),
+		});
+		let contractHash = 'ccb576d6ce6dec84a551e48f0d0b7af89ddba44c7390b690036257a04a3ae9ea';
+		if (network === 'casper-test') {
+			contractHash = '93d923e336b20a4c4ca14d592b60e5bd3fe330775618290104f9beb326db7ae2';
+		}
+		const session = DeployUtil.ExecutableDeployItem.newStoredContractByHash(
+			Uint8Array.from(Buffer.from(contractHash, 'hex')),
+			'undelegate',
+			args,
+		);
+		const deploy = DeployUtil.makeDeploy(deployParams, session, payment);
+		const signedDeploy = DeployUtil.signDeploy(deploy, keyPair);
+		const executionResult = await client.putDeploy(signedDeploy);
+		return executionResult;
+	},
+};
