5d5741e7624b4024f1de2cd5572fe4e1d23f931c
John Gicharu <89821717+gicharujohn@users.noreply.github.com>
2022-06-07 16:20:55 +0300

    chore: data comments

--- src/electron/data/index.cjs
+++ src/electron/data/index.cjs
@@ -1,11 +1,14 @@
 const Store = require('electron-store');
 
+// The encryption key in this case is used to ensure data will be obfuscated and users can't directly edit the config file. This helps in preventing them from directly editing it and resulting in errors in the app
 const store = new Store({
 	accessPropertiesByDotNotation: false,
 	fileExtension: 'gosuto',
 	name: 'gosuto_data',
 	encryptionKey: '@o4EB%Fb&MGFyk^LXj$9Xt5uk94RaR',
 });
+
+// TODO: ADD MIGRATIONS FOR THE DATA ONCE A NEW VERSION HAS BEEN DEPLOYED
 
 module.exports = {
 	saveData: (key, data) => {
