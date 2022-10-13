07393f6a48abd7455151c4475f574cacdccd9c23
John Gicharu <johngicharu@ids.company>
2022-10-13 08:52:20 +0300

    feat: remove dependence on window being active to receive messages

--- src/electron/events/sendMessage.cjs
+++ src/electron/events/sendMessage.cjs
@@ -1,5 +1,9 @@
 const { BrowserWindow } = require('electron');
 
 module.exports = (channel, data) => {
-	return BrowserWindow.getFocusedWindow()?.webContents.send(channel, data);
+	return BrowserWindow.getAllWindows()?.map((window) => {
+		window.webContents.send(channel, data);
+		return window;
+	});
+	// return BrowserWindow.getFocusedWindow()?.webContents.send(channel, data);
 };
